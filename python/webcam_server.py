013090f4fe5be57c742ed91ea8fe1fec53bfa37b
Ron Dagdag <rlyle78@gmail.com>
2016-04-19 11:34:45 -0500

    smaller size and app insight

--- python/webcam_server.py
+++ python/webcam_server.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((TCP_IP, TCP_PORT))
         s.listen(1)
-        
+
         while 1:
             conn, addr = s.accept()
             data = conn.recv(16)
@@ -28,19 +28,24 @@ if __name__ == '__main__':
                     data = conn.recv(buffer)
                     print data
                     flux = data.split(' ')
-                    
+
                     while True:
                         if video.isOpened():
                             success, frame = video.read()
                         else:
-                            frame = np.zeros((500, 500), dtype=np.uint8)
+                            frame = np.zeros((300, 300), dtype=np.uint8)
                             cv2.putText(frame, 'Camera(s) OFF', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                        
-                        frame = imutils.resize(frame, width = 500)
-                        cv2.imshow('image',frame)
-                        if cv2.waitKey(1) & 0xFF == ord('q'):
-                            break
+
+                        frame = imutils.resize(frame, width = 300)
+                        #cv2.imshow('image',frame)
+                        #if cv2.waitKey(1) & 0xFF == ord('q'):
+                        #    break
+                        t = cv2.getTickCount()
                         faces = recognizer.Face.findFrame(frame, flux[0], flux[1], float(flux[2]), int(flux[3]))
+                        t = cv2.getTickCount() - t
+                        print "detection time = %gms" % (t/(cv2.getTickFrequency()*1000.))
+
+                        t = cv2.getTickCount()
                         finds = '{"faces": ['
                         index = 0
                         for data in faces:
@@ -50,7 +55,9 @@ if __name__ == '__main__':
                                 finds += ','
                         finds += ']}'
                         conn.sendall('%s' % finds)
-                        
+                        t = cv2.getTickCount() - t
+                        print "send time = %gms" % (t/(cv2.getTickFrequency()*1000.))
+
                 if data.startswith(FACE_DETECTION):
                     buffer = int(data[6:])
                     conn.send('BUFFER OK')
@@ -58,19 +65,24 @@ if __name__ == '__main__':
                     data = conn.recv(buffer)
                     print data
                     flux = data.split(' ')
-                    
+
                     while True:
                         if video.isOpened():
                             success, frame = video.read()
                         else:
-                            frame = np.zeros((500, 500), dtype=np.uint8)
+                            frame = np.zeros((300, 300), dtype=np.uint8)
                             cv2.putText(frame, 'Camera(s) OFF', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                        
-                        frame = imutils.resize(frame, width = 500)
-                        cv2.imshow('image',frame)
-                        if cv2.waitKey(1) & 0xFF == ord('q'):
-                            break
+
+                        frame = imutils.resize(frame, width = 300)
+                        #cv2.imshow('image',frame)
+                        #if cv2.waitKey(1) & 0xFF == ord('q'):
+                        #    break
+                        t = cv2.getTickCount()
                         faces = detector.FaceDetection.findFrame(frame, flux[0], float(flux[1]), int(flux[2]))
+                        t = cv2.getTickCount() - t
+                        print "detection time = %gms" % (t/(cv2.getTickFrequency()*1000.))
+
+                        t = cv2.getTickCount()
                         finds = '{"faces": ['
                         index = 0
                         for (x, y, w, h) in faces:
@@ -80,6 +92,9 @@ if __name__ == '__main__':
                                 finds += ','
                         finds += ']}'
                         conn.sendall('%s' % finds)
+                        t = cv2.getTickCount() - t
+                        print "send time = %gms" % (t/(cv2.getTickFrequency()*1000.))
+                        
             except:
                 message = '%s' % sys.exc_value
                 message = message.replace('\\', '/').replace('\n', '').replace('\r', '')
